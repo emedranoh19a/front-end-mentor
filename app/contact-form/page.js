697e2973d1308bfe84d38f0a42f4bf0ecee915c3
@WSS-Eric <e-medrano@ws-sol.co.jp>
2024-10-09 13:19:03 +0900

    Set current challenges to work with

--- app/contact-form/page.js
+++ app/contact-form/page.js
@@ -10,8 +10,13 @@ function Grid({ children }) {
 
 function Page() {
   //State
-  const formMethods = useForm();
-  const { handleSubmit, reset } = formMethods; //TODO we might need errors here
+  const formMethods = useForm({ mode: "onChange" });
+  const {
+    handleSubmit,
+    reset,
+    trigger,
+    formState: { errors },
+  } = formMethods; //TODO we might need errors here
 
   //   const [status, setStatus] = useState(null);
   //   const [error, setError] = useState(null);
@@ -22,26 +27,44 @@ function Page() {
     event.preventDefault();
     //TODO Get the errors, return if there are some
     //TODO
-    try {
-      //   setStatus("pending");
-      //   setError(null);
-      const myForm = event.target;
-      const formData = new FormData(myForm);
-      const res = await fetch("/__forms.html", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(formData).toString(),
-      });
-      if (res.status === 200) {
-        // setStatus("ok");
-      } else {
-        // setStatus("error");
-        // setError(`${res.status} ${res.statusText}`);
-      }
-    } catch (e) {
-      //   setStatus("error");
-      //   setError(`${e}`);
+    trigger();
+
+    if (Object.keys(errors).length > 0) {
+      console.log("Errors!");
+      console.log(errors);
+      return;
     }
+    //1. Sending the request to netlify.
+    //This works!
+    // try {
+    //   //   setStatus("pending");
+    //   //   setError(null);
+    //   const myForm = event.target;
+    //   const formData = new FormData(myForm);
+    //   //Production environment
+
+    //   const res = await fetch("/__forms.html", {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    //     body: new URLSearchParams(formData).toString(),
+    //   });
+    //   if (res.status === 200) {
+    //     // setStatus("ok");
+    //     console.log("form submitted");
+    //   } else {
+    //     console.log("Something happened!");
+    //     // setStatus("error");
+    //     // setError(`${res.status} ${res.statusText}`);
+    //   }
+    // } catch (e) {
+    //   console.log(e);
+    //   //   setStatus("error");
+    //   //   setError(`${e}`);
+    // }
+
+    //TODO: 2. Writing to Google Spreadsheet
+    //TODO: 3. Sending email to Maikagura
+    //TODO: 4 Sending a marked up email to the user (a feedback bot.)
   };
 
   //TODO ideas de API. (TODAS deberían resultar)
@@ -66,9 +89,21 @@ function Page() {
             {/* TODO we may want to build static files. build: next build && next export */}
             <h1 className="text-emerald-950 text-3xl font-bold">Contact Us</h1>
             <Grid>
-              <FieldGroup fieldName="firstName" label="First Name" />
-              <FieldGroup fieldName="lastName" label="Last Name" />
-              <FieldGroup fieldName="email" label="Email Address" />
+              <FieldGroup
+                fieldName="firstName"
+                label="First Name"
+                validations={{ required: "this field is required" }}
+              />
+              <FieldGroup
+                fieldName="lastName"
+                label="Last Name"
+                validations={{ required: "this field is required" }}
+              />
+              <FieldGroup
+                fieldName="email"
+                label="Email Address"
+                validations={{ required: "this field is required" }}
+              />
             </Grid>
             <button
               type="submit"
--- app/page.js
+++ app/page.js
@@ -1,8 +1,138 @@
 function MentorCard() {
   //TODO make a proper navigation. This project is only meant to demonstrate copying skills. It deserves a single netlify thread
-  return <div>A link for the page</div>;
+  return <>A link for the page</>;
 }
-const challenges = [{ link: "" }];
+const challenges = [
+  //Newbie
+  //TODO: Recipe page
+  //TODO: Social links profile 
+  //TODO: Blog preview card
+  //TODO: FAQ accordion
+  //TODO: Workit landing page
+  //TODO: Results summary component
+  //TODO: Product preview card
+  //TODO: Skilled e-learning landing page
+  //TODO: Interactive rating component
+  //TODO: QR code component
+  //TODO: NFT preview card comonent
+  //TODO: Equalizer landing page
+  //TODO: Order summary component
+  //TODO: Stats preview card component
+  //TODO: Meet landing page
+  //TODO: 3-column preview card component
+  //TODO: Pod request access landing page
+  //TODO: Profile card component
+  //TODO: Social proof section
+  //TODO: Article preview commponent
+  //TODO: Four cad feature section
+  //TODO: Base Apparel coming soon page
+  //TODO: Introcomponent with sign-up form
+  //TODO: Single price grid component
+  //TODO: Ping single column coming soon page
+  //TODO: Huddle landing page with a single introductory section
+
+  //Junior
+  //TODO: Bento grid
+  //TODO: Product list with cart
+  //TODO: Mortgage repayment calculator
+  //TODO: Contact form
+  //TODO: Newsletter sign-up form with success message.
+  //TODO: Body Mass Index calculator
+  //TODO: Age calculator app
+  //TODO: Single-page deveoper portfolio
+  //TODO: News homepage
+  //TODO: Notifications page
+  //TODO: Interactive card details form
+  //TODO: Single-page design portfolio
+  //TODO: Expenses chart component
+  //TODO: Intro section with dropdownn navigation
+  //TODO: Advice generator app
+  //TODO: Suite landing page
+  //TODO: Art gallery website
+  //TODO: Time tracking dashboard
+  //TODO: GitHub user search app
+  //TODO: Tip calculator app
+  //TODO: Agency landing page
+  //TODO: Typemaster pre-launch landing page
+  //TODO: Blogr landing page
+  //TODO: Crowdfuncing product page
+  //TODO: Interactive pricing component
+  //TODO: Maker pre-launch landing page
+  //TODO: Loopstudios landing page
+  //TODO: Officelite coming soon site
+  //TODO: Testimonials grid section
+  //TODO: Fylo data storage component
+  //TODO: Coding bootcamp testimonials slider
+  //TODO: Social media dashboard with theme switcher
+  //TODO: Insure landing page
+  //TODO: Pricing component with toggle
+  //TODO: Project tracking intro component
+  //TODO: Clipboard landing page
+  //TODO: Huddle landing page with alternating feature blocks
+  //TODO: Fylo dark theme landing page
+  //TODO: Fylo landing page with two column layout
+  //TODO: Huddle landing page with curved sections
+
+  //Intermediate
+  //TODO: Frontend Quiz app
+  //TODO: Dictionary web app
+  //TODO: Password generator app
+  //TODO: In-browser markdown editor
+  //TODO: Itc Tac Toe game
+  //TODO: Interactive comments section
+  //TODO: Space tourism multi-page website
+  //TODO: E-commerce product page
+  //TODO: Galleria slideshow site
+  //TODO: Planets fact site
+  //TODO: Calculator app
+  //TODO: coffeeroasters subscription site
+  //TODO: Launch cocuntdown timer
+  //TODO: Creatuve agency single-page site
+  //TODO: Todo app
+  //TODO: Clock app
+  //TODO: Room homepage
+  //TODO: IP Address Tracker
+  //TODO: Chat app CSS illustration
+  //TODO: Scoot multi-page website
+  //TODO: Dine restaurant website
+  //TODO: Minimalist portfolio website
+  //TODO: Job listings with filtering
+  //TODO: Easybank landing page
+  //TODO: Manage landing page
+  //TODO: URL shortening API landing
+  //TODO: Bookmark landing page
+
+  //Advanced.
+  //TODO: Hangmman game
+  //TODO: Multi-step form
+  //TODO: Connect Four game
+  //TODO:  Entertainment web app
+  //TODO: Memory game
+  //TODO: Pomodoro app
+  //TODO: Devjobs web app
+  //TODO: API multi-page website
+  //TODO: Photosnap multi-page website
+  //TODO: Arch Studio mulkti-page website
+  //TODO: myteam multi-page website
+  //TODO: Rock, Paper, Scissors game
+  //TODO: REST Countries API with color theme witcher
+
+  //Guru: (Paid version) only if this actually works
+  //TODO: Personal finance app
+  //TODO: Link-sharing app
+  //TODO: Kanban task management web app
+  //TODO: Product feedback app
+  //TODO: audiophile e-commerce website
+  //TODO: Invoice app
+  //TODO: Designo multi-page website
+  
+  { link: "/recipe-page", img:"" },
+  { link: "/profile-links", img:"" },
+  { link: "", img:"" },
+  { link: "", img:"" },
+  { link: "", img:"" },
+  { link: "", img:"" },
+  { link: "", img:"" }];
 export default function Home() {
   return (
     <div className="">
